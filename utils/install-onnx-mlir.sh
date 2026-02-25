0723119a4bd251c8fc4a01f7e75f1191dec793b7
lifan <lifan@yupan.ai>
2026-02-25 16:46:36 +0800

    change llvm dependency dir.

--- utils/install-onnx-mlir.sh
+++ utils/install-onnx-mlir.sh
@@ -1,7 +1,11 @@
-# MLIR_DIR must be set with cmake option now
-# MLIR_DIR=$(pwd)/llvm-project/build/lib/cmake/mlir
-MLIR_DIR=/home/lifan/workspace/buddy-mlir/llvm/build/lib/cmake/mlir
-LLVM_DIR=/home/lifan/workspace/buddy-mlir/llvm/build/lib/cmake/llvm
+LLVM_PROJECT_DIR=$(pwd)/../../llvm-project
+LLVM_PROJECT_INSTALL_DIR=$(pwd)/../../install/llvm-project
+
+MLIR_DIR=${LLVM_PROJECT_DIR}/build/lib/cmake/mlir
+LLVM_DIR=${LLVM_PROJECT_DIR}/build/lib/cmake/llvm
+
+echo "MLIR_DIR: ${MLIR_DIR}"
+echo "LLVM_DIR: ${LLVM_DIR}"
 
 mkdir -p build
 cd build
@@ -11,13 +15,13 @@ cmake -G Ninja \
       -DCMAKE_CXX_COMPILER=clang++ \
       -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_ENABLE_ASSERTIONS=ON \
-      -DONNX_MLIR_ENABLE_STABLEHLO=ON \
+      -DONNX_MLIR_ENABLE_STABLEHLO=OFF \
       -DMLIR_DIR=${MLIR_DIR} \
       -DLLVM_DIR=${LLVM_DIR} \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       ..
 
-cmake --build . 
+# cmake --build . 
 
 
 
