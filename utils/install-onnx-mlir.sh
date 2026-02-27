6425dc8aeb7e2963c8bb9e6f43aa8197ebcb43cc
lifan <lifan@yupan.ai>
2026-02-27 14:41:55 +0800

    add install process.

--- utils/install-onnx-mlir.sh
+++ utils/install-onnx-mlir.sh
@@ -1,11 +1,12 @@
 LLVM_PROJECT_DIR=$(pwd)/../../llvm-project
-LLVM_PROJECT_INSTALL_DIR=$(pwd)/../../install/llvm-project
+INSTALL_DIR=$(pwd)/../../install
 
 MLIR_DIR=${LLVM_PROJECT_DIR}/build/lib/cmake/mlir
 LLVM_DIR=${LLVM_PROJECT_DIR}/build/lib/cmake/llvm
 
 echo "MLIR_DIR: ${MLIR_DIR}"
 echo "LLVM_DIR: ${LLVM_DIR}"
+echo "INSTALL_DIR: ${INSTALL_DIR}"
 
 mkdir -p build
 cd build
@@ -18,31 +19,13 @@ cmake -G Ninja \
       -DONNX_MLIR_ENABLE_STABLEHLO=OFF \
       -DMLIR_DIR=${MLIR_DIR} \
       -DLLVM_DIR=${LLVM_DIR} \
+      -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       ..
 
 ninja 
 
-
-
-# mkdir onnx-mlir/build && cd onnx-mlir/build
-# if [[ -z "$pythonLocation" ]]; then
-#   cmake -G Ninja \
-#         -DCMAKE_CXX_COMPILER=/usr/bin/c++ \
-#         -DCMAKE_BUILD_TYPE=Release \
-#         -DLLVM_ENABLE_ASSERTIONS=ON \
-#         -DMLIR_DIR=${MLIR_DIR} \
-#         ..
-# else
-#   cmake -G Ninja \
-#         -DCMAKE_CXX_COMPILER=/usr/bin/c++ \
-#         -DCMAKE_BUILD_TYPE=Release \
-#         -DLLVM_ENABLE_ASSERTIONS=ON \
-#         -DPython3_ROOT_DIR=$pythonLocation \
-#         -DMLIR_DIR=${MLIR_DIR} \
-#         ..
-# fi
-# cmake --build .
+ninja install
 
 # # Run lit tests:
 # export LIT_OPTS=-v
